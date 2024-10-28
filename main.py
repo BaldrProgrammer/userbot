30954eb5bf6e174305ccc65732e4d997f9c8fcbc
balddrAdmin <boliklevik@gmail.com>
2024-10-28 16:24:27 +0100

    userbot +ban +cid +bot

--- main.py
+++ main.py
@@ -6,8 +6,21 @@ api_id = '25836426'
 api_hash = '23fb31c8eea83588c3894a30cda72f7f'
 
 app = Client('account', api_id, api_hash)
-@app.on_message(filters.regex(r'^\.newpasswd'))
+
+
+@app.on_message(filters.regex(r'^\.бот'))
+def cid(client, message):
+    client.send_message(message.chat.id, 'Работает.')
+
+
+@app.on_message(filters.regex(r'^\.сид'))
+def cid(client, message):
+    client.send_message(message.chat.id, f'`{message.chat.id}`', parse_mode=ParseMode.MARKDOWN)
+
+
+@app.on_message(filters.regex(r'^\.\+пароль'))
 def newpasswd(client, message):
+    print('хуй')
     if message.from_user.id == client.get_me().id:
         args = message.text.split()
         with open('storage/passwords.json', 'r', encoding='utf-8') as file:
@@ -19,7 +32,7 @@ def newpasswd(client, message):
         client.send_message(message.chat.id, f'Создан/перезаписан пароль {args[1]} на "{args[2]}"')
 
 
-@app.on_message(filters.regex(r'^\.delpasswd'))
+@app.on_message(filters.regex(r'^\.\-пароль'))
 def delpasswd(client, message):
     if message.from_user.id == client.get_me().id:
         args = message.text.split()
@@ -35,7 +48,7 @@ def delpasswd(client, message):
             client.send_message(message.chat.id, 'Такого пароля не сохранено.')
 
 
-@app.on_message(filters.regex(r'^\.passwd'))
+@app.on_message(filters.regex(r'^\.пароль'))
 def passwd(client, message):
     if message.from_user.id == client.get_me().id:
         args = message.text.split()
@@ -45,8 +58,20 @@ def passwd(client, message):
             text = 'Вот список ключей по которым доступны ваши пароли:\n'
             for password in passwords:
                 text += f'`{password}`' + '\n'
-            client.send_message(message.chat.id, text, disable_web_page_preview=True, parse_mode=ParseMode.MARKDOWN)
+            client.send_message(message.chat.id, text, parse_mode=ParseMode.MARKDOWN)
         else:
             password = args[1]
-            client.send_message(message.chat.id, f'`{passwords[password]}`', disable_web_page_preview=True, parse_mode=ParseMode.MARKDOWN)
+            client.send_message(message.chat.id, f'`{passwords[password]}`', parse_mode=ParseMode.MARKDOWN)
+
+
+@app.on_message(filters.regex(r'^\.бан'))
+def ban(client, message):
+    cid = message.chat.id
+    uid = message.from_user.id
+    if uid == client.get_me().id:
+        user_status = client.get_chat_member(cid, uid)
+        if user_status.privileges.can_restrict_members:
+            if message.reply_to_message:
+                client.ban_chat_member(cid, message.reply_to_message.from_user.id)
+
 app.run()
